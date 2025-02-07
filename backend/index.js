4577ef5158b9022a42117d76242f67945d123003
sarvesh123githubacc <sarveshgarg197@gmail.com>
2025-02-07 21:16:44 +0530

    Edit profile page done, but check it once

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,19 @@ import userRouter from "./routes/users.js";
 import { connectMongo } from "./connection.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = 8080
 
 connectMongo("mongodb://127.0.0.1:27017/hacktu");
 
 app.use(express.json());
 app.use("/users", userRouter);
 
-app.listen(PORT);
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+
+// app.listen(PORT);
